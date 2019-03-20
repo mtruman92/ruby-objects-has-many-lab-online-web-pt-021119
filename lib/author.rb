2c86c3a1d500114b2d0c5c0d2f2897c08c478f7e
Mickae Truman <mtruman@nyit.edu>
2019-03-20 21:59:17 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,7 +16,7 @@ class Author
     post.author = self
   end
   
-  def post_by_title(post_title)
+  def add_post_by_title(post_title)
     post = Post.new(post_title)
     post_title.author = self
   end
