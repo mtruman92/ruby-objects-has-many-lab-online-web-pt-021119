fb09d2d3e2615bee3d1a8eb2b498efa8a31e3af6
Mickae Truman <mtruman@nyit.edu>
2019-03-20 21:56:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,11 +16,14 @@ class Artist
   end
   
   def add_song_by_name(song_name)
+    song = Song.new(song_name)
     @songs << song 
     song.artist = self
   end 
   
   def song_count
+    Song.all.count 
+  end
     
   end
     
